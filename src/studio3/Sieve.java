3d8402b94869528248f85c7b35fd50169f788b44
fareeha <fareeha@localhost>
2022-09-22 14:21:36 -0500

    version 2

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -11,26 +11,42 @@ public class Sieve {
 		int n = in.nextInt(); 
 		boolean prime[] = new boolean [n];
 
+		for (int m = 0; m < n; m++) {
+			prime[m] = true;
+			System.out.println(prime[m]);
+		}
+
 		for (int i = 1; i < n; i++) {
 			for (int j = 2; j < n; j++) {
-				if ((i%j != 0)&&(i != j)){
-					prime[j] = true;
+				if ((i%j == 0)&&(i != j)){
+					prime[j] = false;
 				}
-
-				//				for (int j = 0; j < n; j ++) {
-				//					j = (i * i) + (j * i);
-				//					prime[j] = false;
-				//					System.out.println(prime[j]);
-				//
-				//				}
 			}
-
 		}
-		for (int i = 0;i < n;i++)
-		{
-			if (prime[i] == true) {
-				System.out.println(i);
-			}
+			for (int i = 0;i < n;i++)
+				{
+					if (prime[i] == true) {
+						System.out.println(i);
+					}
 		}
 	}
 }
+		
+
+				//				for (int j = 0; j < n; j ++) {
+////				//					j = (i * i) + (j * i);
+////				//					prime[j] = false;
+////				//					System.out.println(prime[j]);
+////				//
+////				//				}
+////			}
+////
+////		}
+//		for (int i = 0;i < n;i++)
+//		{
+//			if (prime[i] == true) {
+//				System.out.println(i);
+//		}
+//		}
+//	}
+//}
