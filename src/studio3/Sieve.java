005ea13fa73c51b98ba6c12c8e675d81438961bb
fareeha <fareeha@localhost>
2022-09-22 14:16:19 -0500

    didnt finish :(

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,36 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("What do you want the highest number to be?");
+		int n = in.nextInt(); 
+		boolean prime[] = new boolean [n];
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 2; j < 8; j++) {
+				if ((i%j != 0)&&(i != j)){
+					prime[j] = true;
+				}
+
+				//				for (int j = 0; j < n; j ++) {
+				//					j = (i * i) + (j * i);
+				//					prime[j] = false;
+				//					System.out.println(prime[j]);
+				//
+				//				}
+			}
+
+		}
+		for (int i = 0;i < n;i++)
+		{
+			if (prime[i] == true) {
+				System.out.println(i);
+			}
+		}
+	}
+}
